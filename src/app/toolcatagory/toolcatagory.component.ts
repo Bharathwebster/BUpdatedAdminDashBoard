4c0458c3f8b7c9ea08569a2afed5182959843d5b
rameshspujari <rameshpujari165@gmail.com>
2022-02-19 23:38:15 +0530

    first commit

--- src/app/toolcatagory/toolcatagory.component.ts
+++ src/app/toolcatagory/toolcatagory.component.ts
@@ -15,8 +15,8 @@ export class ToolcatagoryComponent implements OnInit {
 
   
 //   idToken = localStorage.getItem('csrftoken');
-apiRoot: string = "http://152.135.122.61:8871";
-//  apiRoot: string = "http://152.135.122.61:8871";
+apiRoot: string = "http://127.0.0.1:8001";
+//  apiRoot: string = "http://127.0.0.1:8001";
 //options = { headers: new HttpHeaders({'Authorization': 'JWT '+ this.cookie.get('csrftoken')}) };
   
   constructor(private _data1:DataService,private http:HttpClient,) { }
